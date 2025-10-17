e6c1be94f1e1c6685f41bc653b4ea3574cdbc608
annagregori <gruppounitelma@gmail.com>
2025-10-17 15:49:18 +0200

    Add BOT_TOKEN and other environment variables

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if os.getenv("RAILWAY_ENVIRONMENT") is None:
     from dotenv import load_dotenv
     load_dotenv()
 
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
